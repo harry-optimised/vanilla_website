11b02a97cec896058bc8537b1a3c7b2569a9c89c
Henry Turner <harry.turner@rovco.com>
2020-12-06 12:01:45 +0000

    Smaller text.

--- js/Navbar.js
+++ js/Navbar.js
@@ -7,12 +7,12 @@ class Navbar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="nav-bar">
-          <div class="nav-item"><h1><a href="/vanilla_website/index.html">Home</a></h1></div>
-          <div class="nav-item"><h1><a href="/vanilla_website/about.html">About</a></h1></div>
-          <div class="nav-item"><h1><a href="/vanilla_website/machine_learning.html">Machine Learning</a></h1></div>
-          <div class="nav-item"><h1><a href="/vanilla_website/books.html">Books</a></h1></div>
-          <div class="nav-item"><h1><a href="/vanilla_website/blog.html">Blog Posts</a></h1></div>
-          <div class="nav-item"><h1><a href="/vanilla_website/projects.html">Projects</a></h1></div>
+          <div class="nav-item"><h1><a href="/index.html">Home</a></h1></div>
+          <div class="nav-item"><h1><a href="/about.html">About</a></h1></div>
+          <div class="nav-item"><h1><a href="/machine_learning.html">Machine Learning</a></h1></div>
+          <div class="nav-item"><h1><a href="/books.html">Books</a></h1></div>
+          <div class="nav-item"><h1><a href="/blog.html">Blog Posts</a></h1></div>
+          <div class="nav-item"><h1><a href="/projects.html">Projects</a></h1></div>
       </div>
     `
   }
