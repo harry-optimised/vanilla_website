a138138da63e9e1c7452005a92e78917e0dfbd79
Harry Turner <harry.turner@rovco.com>
2022-01-03 09:08:55 +0000

    Removed books, updated about.

--- js/Navbar.js
+++ js/Navbar.js
@@ -10,7 +10,6 @@ class Navbar extends HTMLElement {
           <div class="nav-item"><h1><a href="/index.html">Home</a></h1></div>
           <div class="nav-item"><h1><a href="/about.html">About</a></h1></div>
           <div class="nav-item"><h1><a href="/machine_learning.html">Machine Learning</a></h1></div>
-          <div class="nav-item"><h1><a href="/books.html">Books</a></h1></div>
           <div class="nav-item"><h1><a href="/blog.html">Blog Posts</a></h1></div>
           <div class="nav-item"><h1><a href="/projects.html">Projects</a></h1></div>
       </div>
