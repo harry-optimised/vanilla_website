d818f25b793389fc1b5d5c9b69e7b5b9d1182a9c
Harry Turner <harry.turner@rovco.com>
2022-01-03 09:38:07 +0000

    Refactor and reorganisation.

--- js/Navbar.js
+++ js/Navbar.js
@@ -8,10 +8,9 @@ class Navbar extends HTMLElement {
     this.innerHTML = `
       <div class="nav-bar">
           <div class="nav-item"><h1><a href="/index.html">Home</a></h1></div>
-          <div class="nav-item"><h1><a href="/about.html">About</a></h1></div>
           <div class="nav-item"><h1><a href="/machine_learning.html">Machine Learning</a></h1></div>
-          <div class="nav-item"><h1><a href="/blog.html">Blog Posts</a></h1></div>
-          <div class="nav-item"><h1><a href="/projects.html">Projects</a></h1></div>
+          <div class="nav-item"><h1><a href="/system.html">System Design</a></h1></div>
+          <div class="nav-item"><h1><a href="/blog.html">Blog</a></h1></div>
       </div>
     `
   }
