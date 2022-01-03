800ba5777d3f28b3a69da323b546bb546478df50
Harry Turner <harry.turner@rovco.com>
2022-01-03 12:54:21 +0000

    Added python_project article.

--- js/Navbar.js
+++ js/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends HTMLElement {
       <div class="nav-bar">
           <div class="nav-item"><h1><a href="/index.html">Home</a></h1></div>
           <div class="nav-item"><h1><a href="/machine_learning.html">Machine Learning</a></h1></div>
-          <div class="nav-item"><h1><a href="/system.html">System Design</a></h1></div>
+          <div class="nav-item"><h1><a href="/system_design.html">System Design</a></h1></div>
           <div class="nav-item"><h1><a href="/blog.html">Blog</a></h1></div>
       </div>
     `
