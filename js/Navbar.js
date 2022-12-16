fd69573846ca3022cfec6cdbc93087c0f2c44638
Harry Turner <harry.turner@rovco.com>
2022-12-16 22:23:06 +0000

    Added CV to website.

--- js/Navbar.js
+++ js/Navbar.js
@@ -11,6 +11,7 @@ class Navbar extends HTMLElement {
           <div class="nav-item"><h1><a href="/machine_learning.html">Machine Learning</a></h1></div>
           <div class="nav-item"><h1><a href="/system_design.html">System Design</a></h1></div>
           <div class="nav-item"><h1><a href="/blog.html">Blog</a></h1></div>
+          <div class="nav-item"><h1><a href="/cv.html">CV</a></h1></div>
       </div>
     `
   }
