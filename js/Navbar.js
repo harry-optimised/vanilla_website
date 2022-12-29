734e699ab4a69d3a590cd2870699bf873fa2ca86
Harry Turner <harry.turner@rovco.com>
2022-12-29 20:01:12 +0000

    Condense blog to index page.

--- js/Navbar.js
+++ js/Navbar.js
@@ -8,10 +8,7 @@ class Navbar extends HTMLElement {
     this.innerHTML = `
       <div class="nav-bar">
           <div class="nav-item"><h1><a href="/index.html">Home</a></h1></div>
-          <div class="nav-item"><h1><a href="/machine_learning.html">Machine Learning</a></h1></div>
-          <div class="nav-item"><h1><a href="/system_design.html">System Design</a></h1></div>
-          <div class="nav-item"><h1><a href="/blog.html">Blog</a></h1></div>
-          <div class="nav-item"><h1><a href="/cv.html">CV</a></h1></div>
+          <div class="nav-item"><h1><a href="https://drive.google.com/file/d/11anz-75-LU9RDeFTEp7I4waJ9JW7eCWd/view?usp=share_link">CV</a></h1></div>
       </div>
     `
   }
