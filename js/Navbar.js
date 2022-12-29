1a33d3a0b0436c91c1a5e8d2be59967b089d0d02
Harry Turner <harry.turner@rovco.com>
2022-12-29 20:32:47 +0000

    Fix logo link.

--- js/Navbar.js
+++ js/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends HTMLElement {
     this.innerHTML = `
       <div class="nav-bar">
         <div class="nav-bar-left">
-          <a href="/index.html"><img src="images/logo.png" alt="harry turner logo"/></a>
+          <a href="/index.html"><img src="/images/logo.png" alt="harry turner logo"/></a>
           <div class="nav-item"><h1><a href="/index.html">Home</a></h1></div>
           <div class="nav-item"><h1><a href="/machine_learning.html">ML</a></h1></div>
           <div class="nav-item"><h1><a href="https://drive.google.com/file/d/11anz-75-LU9RDeFTEp7I4waJ9JW7eCWd/view?usp=share_link">CV</a></h1></div>
