a8cfd8c4c3eb6fe99ada60a5693e6a65a67ff9da
harry-optimised <henry.j.turner@gmail.com>
2023-08-06 06:58:04 +0100

    Revamped the style of the website.

--- js/Navbar.js
+++ js/Navbar.js
@@ -8,12 +8,11 @@ class Navbar extends HTMLElement {
     this.innerHTML = `
       <div class="nav-bar">
         <div class="nav-bar-left">
-          <a href="/index.html"><img src="/images/logo.png" alt="harry turner logo"/></a>
+          <div class="nav-item"><h1 style="font-weight: 1000; font-size: 200%"><a href="/index.html">Harrys Blog</a></h1></div>
           <div class="nav-item"><h1><a href="/index.html">Home</a></h1></div>
-          <div class="nav-item"><h1><a href="/machine_learning.html">ML</a></h1></div>
           <div class="nav-item"><h1><a href="https://drive.google.com/file/d/11anz-75-LU9RDeFTEp7I4waJ9JW7eCWd/view?usp=share_link">CV</a></h1></div>
         </div>
-        <a href="https://www.linkedin.com/in/harry-j-turner/"><img style="width: 24px" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="harry turner logo"/></a>     
+        <a href="https://www.linkedin.com/in/harry-j-turner/"><img style="width: 24px; margin-right: 16px" src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" alt="harry turner logo"/></a>     
       </div>
     `
   }
