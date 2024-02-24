d2435d28ec364f0b7329cde5e423aaca7c02f730
Harry <henry.j.turner@gmail.com>
2024-02-24 10:29:40 +0000

    Long overdue update.

--- js/Navbar.js
+++ js/Navbar.js
@@ -8,9 +8,8 @@ class Navbar extends HTMLElement {
     this.innerHTML = `
       <div class="nav-bar">
         <div class="nav-bar-left">
-          <div class="nav-item"><h1 style="font-weight: 1000; font-size: 200%"><a href="/index.html">Harrys Blog</a></h1></div>
-          <div class="nav-item"><h1><a href="/index.html">Home</a></h1></div>
-          <div class="nav-item"><h1><a href="https://drive.google.com/file/d/11anz-75-LU9RDeFTEp7I4waJ9JW7eCWd/view?usp=share_link">CV</a></h1></div>
+          <div class="nav-item"><h1 style="font-weight: 1000; font-size: 200%"><a href="/index.html">Harrys Projects</a></h1></div>
+          
         </div>
         <a href="https://www.linkedin.com/in/harry-j-turner/"><img style="width: 24px; margin-right: 16px" src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" alt="harry turner logo"/></a>     
       </div>
